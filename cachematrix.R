f4de6cd9b0dd9c0e85c895d74e2610eded2a97c8
GabrielGutfreund <gabriel.gutfreund@laposte.net>
2014-07-26 21:51:32 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,24 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) 
+makeCacheMatrix <- function(x = matrix())
 {
-
+        nb_col <- ncol(x)
+        nb_row <- nrow(x)
+        if(nb_col == nb_row)
+        {
+                det_x <- det(x)
+                
+                if (det_x != 0)
+                {
+                        det_x
+                }else{
+                        print("Warning! This function can work only when the determinant of the matrix isn't null (O).")
+                }                
+        }else{
+                print("Warning! This function can work only with square matrix.")
+        }
+        
 }
 
 
